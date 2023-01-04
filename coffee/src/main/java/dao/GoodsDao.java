25f2d2eb4d6b92ff8fdc718698abffff1ca097ad
hhj <hh96011@gmail.com>
2023-01-04 17:36:53 +0900

    goodsDao

--- coffee/src/main/java/dao/GoodsDao.java
+++ coffee/src/main/java/dao/GoodsDao.java
@@ -1,5 +1,37 @@
 package dao;
 
-public class GoodsDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import vo.Goods;
+
+public class GoodsDao {
+	
+	public ArrayList<Goods> selectListFromGoods(Connection conn, int categoryCode) throws Exception {
+		ArrayList<Goods> list = new ArrayList<Goods>();
+		
+		String sql = "SELECT * FROM goods WHERE category_code = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, categoryCode);
+		
+		ResultSet rs = stmt.executeQuery();
+
+		while(rs.next()) {
+			Goods c = new Goods();
+			c.setCategoryCode(rs.getInt("categoryCode"));
+			
+			list.add(c);
+		}
+		rs.close();
+		stmt.close();				
+		
+		return list;
+		
+	}
+	
+	// insertGoods
+	
 }
+
--- coffee/src/main/java/vo/Goods.java
+++ coffee/src/main/java/vo/Goods.java
@@ -1,5 +1,62 @@
 package vo;
 
 public class Goods {
-
+	private int goodsCode;
+	private String goodsName;
+	private int goodsPrice;
+	private String soldout;
+	private String empId;
+	private int hit;
+	private String createdate;
+	private int categoryCode;
+	public int getGoodsCode() {
+		return goodsCode;
+	}
+	public void setGoodsCode(int goodsCode) {
+		this.goodsCode = goodsCode;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(int goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public String getSoldout() {
+		return soldout;
+	}
+	public void setSoldout(String soldout) {
+		this.soldout = soldout;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public int getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(int categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	
+	
 }
